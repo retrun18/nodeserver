59fd48d962b387164c862064b4c613ee0d47b0af
retrun18 <648437254@qq.com>
2017-10-13 15:37:26 +0800

    update

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ http.createServer(function(req,res)
 {
 	res.writeHead(200,{'Content-Type':'text/plain'})
 	res.end('Hello World\n')
-}).listen(8080,'47.94.207.29')
+}).listen(8080,'0.0.0.0')
 console.log('服务运行在 http://47.94.207.29:8080/')
