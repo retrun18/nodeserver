60d53e6419892ca64dfcadfab531724bddcb5687
retrun18 <648437254@qq.com>
2017-10-13 15:30:34 +0800

    update 2017-10-13 15:30:29

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ http.createServer(function(req,res)
 {
 	res.writeHead(200,{'Content-Type':'text/plain'})
 	res.end('Hello World\n')
-}).listen(8080,'127.0.0.1')
-console.log('服务运行在 http://127.0.0.1:8080/')
+}).listen(8080,'localhost')
+console.log('服务运行在 http://localhost:8080/')
