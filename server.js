c112db01b96859d745e5b7528a7ec0e505eaf275
retrun18 <648437254@qq.com>
2017-10-13 15:27:39 +0800

    update 2017年10月13日15:27:35

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ http.createServer(function(req,res)
 {
 	res.writeHead(200,{'Content-Type':'text/plain'})
 	res.end('Hello World\n')
-}).listen(1337,'127.0.0.1')
-console.log('服务运行在 http://127.0.0.1:1337/')
+}).listen(8080,'127.0.0.1')
+console.log('服务运行在 http://127.0.0.1:8080/')
